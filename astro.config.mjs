263044190db53413ab27a99e66a67375d904240b
omarcarreon <oomarcarreon@gmail.com>
2025-10-18 16:48:02 -0600

    changed base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omarcarreon.github.io',
-  base: import.meta.env.PROD ? '/portfolio' : '/',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
