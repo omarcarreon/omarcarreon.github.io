3aecedd318fa104436ac59861634cadf92c3568b
omarcarreon <oomarcarreon@gmail.com>
2025-10-18 16:55:51 -0600

    set base as /portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omarcarreon.github.io',
-  base: '/',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
